46947c4b5b7c63d04591a385fcca54ac00400114
Brandon Chen <brandon.chen@stonybrook.edu>
2019-05-03 12:39:53 -0400

    Multiple binaries and added correct addresses

--- rop.py
+++ rop.py
@@ -3,14 +3,30 @@ import sys
 
 md = Cs(CS_ARCH_X86, CS_MODE_32)
 
-with open(sys.argv[1], 'rb') as f:
-   instructions = ''
-   while True:
-      curr_byte = f.read(1)
-      if not curr_byte:
-         break
-      instructions += curr_byte
-      if curr_byte == '\xc3':          
-         for i in md.disasm(curr, 0x1000):           
-            print "0x%x:\t%s\t%s" %(i.address, i.mnemonic, i.op_str)
-         instructions = ''
+seen_instructions = set()
+
+class Gadget:
+    def __init__(self, mnemonic, op_str, start_addr, end_addr):
+        self.mnemonic = mnemonic
+        self.op_str = op_str
+        self.start_addr = start_addr
+        self.end_addr = end_addr
+
+def get_gadgets(binaries):
+    for binary in binaries:
+        print('Processing file: ' + binary)
+        with open(binary, 'rb') as f:
+            instructions = b''
+            address = 0x0
+            while True:
+                curr_byte = f.read(1)
+                if not curr_byte:
+                    break
+                instructions += curr_byte
+                if curr_byte == b'\xc3':  
+                    for i in md.disasm(instructions, address):
+                        print ('0x%x:\t%s\t%s' %(i.address, i.mnemonic, i.op_str))
+                        address = i.address + 1
+                    instructions = b''
+
+get_gadgets(sys.argv[1:])
