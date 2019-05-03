8eb93fbc10ce96f18b92e32ae5968af88c23beef
Brandon Chen <brandon.chen@stonybrook.edu>
2019-05-03 13:11:46 -0400

    Gadget collecting finished

--- rop.py
+++ rop.py
@@ -3,8 +3,6 @@ import sys
 
 md = Cs(CS_ARCH_X86, CS_MODE_32)
 
-seen_instructions = set()
-
 class Gadget:
     def __init__(self, mnemonic, op_str, start_addr, end_addr):
         self.mnemonic = mnemonic
@@ -12,21 +10,45 @@ class Gadget:
         self.start_addr = start_addr
         self.end_addr = end_addr
 
+    def __str__(self):
+        return '0x%x:\t0x%x:\t%s\t%s' % (self.start_addr, self.end_addr, self.mnemonic, self.op_str)
+
 def get_gadgets(binaries):
+    seen_instructions = set() # Avoid duplicated instructions
+    mnemonic_to_gadget = {}
+
     for binary in binaries:
         print('Processing file: ' + binary)
         with open(binary, 'rb') as f:
             instructions = b''
-            address = 0x0
+            start_addr = 0x0
+            end_addr = 0x0
             while True:
                 curr_byte = f.read(1)
                 if not curr_byte:
                     break
                 instructions += curr_byte
+                end_addr += 1
                 if curr_byte == b'\xc3':  
-                    for i in md.disasm(instructions, address):
-                        print ('0x%x:\t%s\t%s' %(i.address, i.mnemonic, i.op_str))
-                        address = i.address + 1
-                    instructions = b''
+                    for i in md.disasm(instructions, start_addr):
+                        if (i.mnemonic + ' ' + i.op_str) not in seen_instructions:
+                            gadget = Gadget(i.mnemonic, i.op_str, start_addr, end_addr)
+                            seen_instructions.add(i.mnemonic + ' ' + i.op_str)
+                            
+                            if i.mnemonic not in mnemonic_to_gadget:
+                                mnemonic_to_gadget[i.mnemonic] = []
 
-get_gadgets(sys.argv[1:])
+                            mnemonic_to_gadget[i.mnemonic].append(gadget)              
+                    
+                        start_addr = i.address + 1
+                    instructions = b''
+    
+    return mnemonic_to_gadget
+
+gadgets = get_gadgets(sys.argv[1:])
+
+for k, v in gadgets.items():
+    print('-' * 50)
+    print(k + ':')
+    for gadget in v:
+        print(gadget)
